b8c3e3734e8fe00c5c54fdf7f7b0a774e8a53b04
Vadim Kushir <vadim.kushnir@logity.tech>
2023-03-17 23:49:30 +0600

    feat: refactored 'fetchPhrases' function

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const phrasesElement = document.getElementById("dynamicPhrase");
-  const delay = 3000;
+const fetchPhrases = async(elementId, filename, delay = 3000) => {
+  const phrasesElement = document.getElementById(elementId);
 
-  const fetchPhrases = async() => {
-    try {
-      const response = await fetch("phrases.json");
-      const phrasesJson = await response.json();
-      const phrases = phrasesJson['phrases']
-      let index = 0;
+  try {
+    const response = await fetch(filename);
+    const phrases = await response.json();
+    let index = 0;
 
-      setInterval(() => {
-        phrasesElement.textContent = phrases[index];
-        index = (index + 1) % phrases.length;
-      }, delay);
-    } catch (error) {
-      console.error("Ошибка при загрузке фраз:", error);
-    }
+    setInterval(() => {
+      phrasesElement.textContent = phrases[index];
+      index = (index + 1) % phrases.length;
+    }, delay);
+  } catch (error) {
+    console.error("Ошибка при загрузке фраз:", error);
   }
+}
 
-  fetchPhrases();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchPhrases("dynamicPhrase", "phrases.json");
 });
