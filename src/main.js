a84fa12cdb158abeaaf478a126addf2ac1868c59
Vadim Kushir <vadim.kushnir@logity.tech>
2023-03-18 17:00:53 +0600

    feat: Implement pause/resume on click for phrases and combine JSON files

--- src/main.js
+++ src/main.js
@@ -1,20 +1,47 @@
-const fetchPhrases = async(elementId, filename, delay = 3000) => {
-  const phrasesElement = document.getElementById(elementId);
-
+async function fetchPhrases(elementId, jsonFile, phrasesKey, interval) {
   try {
-    const response = await fetch(filename);
-    const phrases = await response.json();
-    let index = 0;
+    const response = await fetch(jsonFile);
+    const data = await response.json();
+    const phrases = data[phrasesKey];
 
-    setInterval(() => {
-      phrasesElement.textContent = phrases[index];
-      index = (index + 1) % phrases.length;
-    }, delay);
+    if (!phrases) throw new Error(`Phrases not found for key: ${phrasesKey}`);
+
+    const element = document.getElementById(elementId);
+    if (!element) throw new Error(`Element not found for id: ${elementId}`);
+
+    let currentIndex = 0;
+    let timer;
+    let isPaused = false;
+
+    const updatePhrase = () => {
+      element.textContent = phrases[currentIndex];
+      currentIndex = (currentIndex + 1) % phrases.length;
+    };
+
+    const startInterval = () => {
+      timer = setInterval(updatePhrase, interval);
+    };
+
+    const stopInterval = () => {
+      clearInterval(timer);
+    };
+
+    element.addEventListener("click", () => {
+      if (isPaused) {
+        startInterval();
+      } else {
+        stopInterval();
+      }
+      isPaused = !isPaused;
+    });
+
+    startInterval();
   } catch (error) {
-    console.error("Ошибка при загрузке фраз:", error);
+    console.error(`Error in fetchPhrases: ${error}`);
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetchPhrases("dynamicPhrase", "phrases.json");
+  fetchPhrases("dynamicPhrase", "phrases.json", "subheaderPhrases", 4000);
+  fetchPhrases("footerPhrase", "phrases.json", "footerPhrases", 3000);
 });
