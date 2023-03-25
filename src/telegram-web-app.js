d1ceb11f53ede821cab0a0a077147b486ce05d11
Vadim Kushir <vadim.kushnir@logity.tech>
2023-03-26 05:09:50 +0600

    feat: add button to fetch data and update button caption

--- src/telegram-web-app.js
+++ src/telegram-web-app.js
@@ -14,8 +14,37 @@ const createTextElement = (elementId, contentText, fallbackText) => {
       platformInfo.textContent = fallbackText;
     }
   }
-}
+};
+
+const createButton = (buttonText, onClickFunction) => {
+  const button = document.createElement('button');
+  button.textContent = buttonText;
+  document.body.appendChild(button);
+  button.addEventListener('click', onClickFunction);
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  createTextElement('platform-info', `Platform: ${window.Telegram.WebApp.platform}`, 'Platform information not available.')
+  createTextElement(
+    'platform-info',
+    `Platform: ${window.Telegram.WebApp.platform}`,
+    'Platform information not available.'
+  );
+
+  createButton('Press me', () => {
+    fetch('https://a917-5-76-59-196.eu.ngrok.io')
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error('Network response was not ok');
+        }
+      })
+      .then((data) => {
+        const button = document.querySelector('button');
+        button.textContent = data;
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  });
 });
