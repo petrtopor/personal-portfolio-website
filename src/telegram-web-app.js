8e3054eb7ab81893cf9c8cd6cb1a4ac243b7e361
Vadim Kushir <vadim.kushnir@logity.tech>
2023-03-26 04:29:37 +0600

    feat: implemented 'text_color' & 'bg_color' displaying

--- src/telegram-web-app.js
+++ src/telegram-web-app.js
@@ -1,4 +1,23 @@
+import './telegram-web-app.scss';
+
+const createTextElement = (elementId, contentText, fallbackText) => {
+  const existingPlatformInfo = document.getElementById(elementId);
+
+  if (!existingPlatformInfo) {
+    const platformInfo = document.createElement('div');
+    platformInfo.id = elementId;
+    document.body.appendChild(platformInfo);
+
+    if (window.Telegram && window.Telegram.WebApp) {
+      platformInfo.textContent = contentText;
+    } else {
+      platformInfo.textContent = fallbackText;
+    }
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+  /*
   const existingPlatformInfo = document.getElementById('platform-info');
 
   if (!existingPlatformInfo) {
@@ -12,4 +31,9 @@ document.addEventListener('DOMContentLoaded', function () {
       platformInfo.textContent = 'Platform information not available.';
     }
   }
+  */
+
+  createTextElement('platform-info', `Platform: ${window.Telegram.WebApp.platform}`, 'Platform information not available.')
+  createTextElement('text_color', `text_color: ${window.Telegram.WebApp.ThemeParams.text_color}`, 'text_color information not available.')
+  createTextElement('bg_color', `bg_color: ${window.Telegram.WebApp.ThemeParams.bg_color}`, 'bg_color information not available.')
 });
