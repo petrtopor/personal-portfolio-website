ecab30e718819dbdf9a001a670715feb8073e602
Vadim Kushir <vadim.kushnir@logity.tech>
2023-03-26 04:50:15 +0600

    fix: removed unnecessary elements creation

--- src/telegram-web-app.js
+++ src/telegram-web-app.js
@@ -17,23 +17,5 @@ const createTextElement = (elementId, contentText, fallbackText) => {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  /*
-  const existingPlatformInfo = document.getElementById('platform-info');
-
-  if (!existingPlatformInfo) {
-    const platformInfo = document.createElement('div');
-    platformInfo.id = 'platform-info';
-    document.body.appendChild(platformInfo);
-
-    if (window.Telegram && window.Telegram.WebApp) {
-      platformInfo.textContent = `Platform: ${window.Telegram.WebApp.platform}`;
-    } else {
-      platformInfo.textContent = 'Platform information not available.';
-    }
-  }
-  */
-
   createTextElement('platform-info', `Platform: ${window.Telegram.WebApp.platform}`, 'Platform information not available.')
-  createTextElement('text_color', `text_color: ${window.Telegram.WebApp.ThemeParams.text_color}`, 'text_color information not available.')
-  createTextElement('bg_color', `bg_color: ${window.Telegram.WebApp.ThemeParams.bg_color}`, 'bg_color information not available.')
 });
